651e8e022f546048f03640d17e63ebd5c469ce90
dviramontes <dviramontes@gmail.com>
2014-07-20 08:57:29 -0600

    plucks catergory data from set

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
+	// DONE : load .csv data
+	// TODO : # of violentions in each category
+
 	// uses TABLETOP.JS to serve data from google spreadsheets
 	var spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1gfPjLfIJHXSH5plx-FgThng0S1t9JKIfujm_9KK-V5w/pubhtml"
 
@@ -10,19 +13,46 @@ $(function(){
 	}
 
 	function draw(data){
+
+		var catergories = _.pluck(data,'violationcategory');
+		var uniqueCategories = _.unique(catergories);
+		var violationCount= []
+
+		_.each(uniqueCategories, function(e){
+			return violationCount.push(new Object({"name": e, "count":0}))
+		})
+
+//		_.each(catergories, function(cat){
+//			if(_.indexOf(uniqueCategories, cat) >= 0){
+//				violationCount.cat.count += 1;
+//			}
+//		})
+
+		console.log("number of unique categoies :: " + uniqueCategories.length)
+		console.dir(uniqueCategories)
+		console.dir(violationCount)
+
+
 		// setup svg
 		d3.select('.jumbotron')
 			.append('svg')
 			.attr('class','viz')
-			.attr('width', '500')
-			.attr('height', '350')
-			.style('background', 'white')
+			.attr('width', 500)
+			.attr('height', 350)
+			//.style('background', '#ccc')
+
+		// shortcuts
+		var svg = d3.select('svg')
+
+		// selectAll + data + enter + append
+		svg
+			.selectAll()
 
 	}
 
 	function tableTopCallback(data, tabletop) {
 		console.info("Successfully processed!")
-		//console.table(data);
+//		console.table(data);
 		draw(data);
 	}
 
